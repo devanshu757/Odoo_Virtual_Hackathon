c18cc749951ec43800b131bf09d0ee77a8459f94
Fenil R Mahesuriya <fenilmahesuriya@gmail.com>
2025-07-12 14:45:23 +0530

    backend part

--- server/models/Swap.js
+++ server/models/Swap.js
@@ -1,12 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const swapSchema = new mongoose.Schema({
-  requester: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  responder: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  itemRequested: { type: mongoose.Schema.Types.ObjectId, ref: 'Item' },
-  itemOffered: { type: mongoose.Schema.Types.ObjectId, ref: 'Item' },
-  status: { type: String, enum: ['pending', 'completed', 'declined'], default: 'pending' },
-  createdAt: { type: Date, default: Date.now }
+const SwapSchema = new Schema ({
+    item: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Item',
+        required: true,
+    },
+    requester: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    ower: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    method: {
+        type: String,
+        enum: ['swap', 'redeem'],
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'accepted', 'rejected', 'completed'],
+        default: 'pending',
+    },
+    message: {
+        type: String,
+    },
+    requestedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    completedAt: {
+        type: Date,
+    },
+
 });
 
-module.exports = mongoose.model('Swap', swapSchema);
+const Swap = mongoose.model("Swap", SwapSchema);
+Swap.createIndexes();
+module.exports = Swap;
