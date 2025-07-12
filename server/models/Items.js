4a068e23ba1fc6fded58c69155ca2c48389c0647
Fenil R Mahesuriya <fenilmahesuriya@gmail.com>
2025-07-12 13:57:21 +0530

    Items.js

--- server/models/Items.js
+++ server/models/Items.js
@@ -1,16 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");;
+const { Schema } = mongoose;
 
-const itemSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  category: String,
-  type: String,
-  size: String,
-  condition: String,
-  tags: [String],
-  images: [String],
-  uploader: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  status: { type: String, default: 'available' }
-});
+const ItemSchema = new Schema ({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    size: {
+        type: String,
+        required: true,
+    },
+    tag: {
+        type: String,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    uploader: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['available', 'swapped', 'pending'],
+        default: 'available',
+    },
+    isApproved: {
+        type: Boolean,
+        default: false,
+    },
+}, {timestamps: true});
 
-module.exports = mongoose.model('Item', itemSchema);
+const Item = mongoose.model("Item", ItemSchema);
+Item.createIndexes();
+module.exports = Item;
